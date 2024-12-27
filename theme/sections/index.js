88f9634ba7d47d6764ec75518eca4a5c4c08bab7
Jigar Dafda <jigar.dafda@gmail.com>
2024-12-27 10:20:07 +0000

    [Auto Generated]

--- theme/sections/index.js
+++ theme/sections/index.js
@@ -8,12 +8,12 @@ import * as FeaturedProductsSection from './featured-products.jsx';
 import * as HeroImageSection from './hero-image.jsx';
 import * as HeroVideoSection from './hero-video.jsx';
 import * as HorizontalBannerSection from './horizontal-banner.jsx';
-import * as ImageGallerySection from './image-gallery.section.jsx';
+import * as ImageGallerySection from './image-gallery.jsx';
 import * as ImageSlideshowSection from './image-slideshow.jsx';
-import * as LinkSection from './link.section.jsx';
+import * as LinkSection from './link.jsx';
 import * as MediaWithTextSection from './media-with-text.jsx';
-import * as RawHtmlSection from './raw-html.section.jsx';
-import * as TestimonialsSection from './testimonials.section.jsx';
+import * as RawHtmlSection from './raw-html.jsx';
+import * as TestimonialsSection from './testimonials.jsx';
 import * as TrustMarkerSection from './trust-marker.jsx';
 
 export default {
